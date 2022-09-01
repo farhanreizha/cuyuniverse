91125a6b314c48c492a9b2fa917c1e3ebf777784
farhanreizha <reizha226@gmail.com>
2022-09-01 23:05:09 +0700

    change posts Teams and postslist

--- resources/js/Components/Homepage/PostsLists.jsx
+++ resources/js/Components/Homepage/PostsLists.jsx
@@ -1,5 +1,5 @@
-import { formatTime } from "@/utils/jsHelper";
-import { Link } from "@inertiajs/inertia-react";
+import {formatTime} from "@/utils/jsHelper";
+import {Link} from "@inertiajs/inertia-react";
 
 const noPosts = () => {
   return (
@@ -14,33 +14,37 @@ const noPosts = () => {
 const isPosts = (posts, from) => {
   return posts.map((post, i) => {
     let more = false;
-    let desc = post.description
+    let desc = post.description;
     if (desc.length > 150) {
-      desc = desc.slice(0, 150)
-      more = true
+      desc = desc.slice(0, 150);
+      more = true;
     }
+
+    console.log(posts);
     return (
       <Link
-        href={`/post/${post.id}`} method="get" as="div"
+        href={`/post/${post.id}`}
+        method="get"
+        as="div"
         key={i}
-        className="card w-full md:w-1/2 mx-auto mb-4 cursor-pointer rounded-md transition ease-in-out duration-400 bg-slate-50 shadow-md hover:-translate-y-1 hover:bg-slate-200 hover:shadow-xl text-black dark:shadow-none dark:bg-slate-500 dark:hover:bg-slate-700 dark:border-0 dark:text-white">
-        {post.image &&
-          // <div class="md:shrink-0">
-          <img src={`/storage/images/posts/${post.image}`} alt="Post Image" className="w-full h-auto mx-auto" />
-          // </div>
-        }
+        className="duration-400 card mx-auto mb-4 w-full cursor-pointer rounded-md bg-slate-50 text-black shadow-md transition ease-in-out hover:-translate-y-1 hover:bg-slate-200 hover:shadow-xl dark:border-0 dark:bg-slate-500 dark:text-white dark:shadow-none dark:hover:bg-slate-700 md:w-[97%]">
+        {post.image && (
+          <div className="md:shrink-0">
+            <img src={`/storage/images/posts/${post.image}`} alt="Post Image" className="mx-auto w-full" />
+          </div>
+        )}
         <div className="card-body">
-          {post.hashtag &&
+          {post.hashtag && (
             <Link className="card-title" as="a" href={`/?tag=${post.hashtag}`}>
               <div className="badge badge-primary text-primary-content">#{post.hashtag}</div>
             </Link>
-          }
-          <div className="text-xl break-words break-normal">
+          )}
+          <div className="break-normal break-words text-xl">
             {desc}
-            {more && <span className="italic text-sm text-primary"> ...lebih lengkap</span>}
+            {more && <span className="text-sm italic text-primary"> ...lebih lengkap</span>}
           </div>
 
-          <div className="mt-2 py-2 flex flex-row items-center">
+          <div className="mt-2 flex flex-row items-center py-2">
             <Link href={`/author/${post.author}`} as="button" method="get" className="avatar">
               <div className="w-8 rounded-full">
                 {from?.page == "author" ? (
--- resources/js/Pages/Posts.jsx
+++ resources/js/Pages/Posts.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Head, Link } from "@inertiajs/inertia-react";
+import React, {useEffect, useRef, useState} from "react";
+import {Head, Link} from "@inertiajs/inertia-react";
 import PostsList from "@/Components/Homepage/PostsLists";
 import Guest from "@/Layouts/Guest";
-import { Inertia } from "@inertiajs/inertia";
-import { FaArrowLeft } from "react-icons/fa";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import {Inertia} from "@inertiajs/inertia";
+import {FaArrowLeft} from "react-icons/fa";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function PostsPage(props) {
-  const { auth, title, tags } = props;
-  const { data: posts, meta, links } = props.posts;
+  const {auth, title, tags} = props;
+  const {data: posts, meta, links} = props.posts;
   const [allPosts, setAllPosts] = useState([...posts]);
-  const [tagParam, setQueryParam] = useState("")
+  const [tagParam, setQueryParam] = useState("");
 
-  const parseParams = (querystring) => {
+  const parseParams = querystring => {
     const params = new URLSearchParams(querystring);
     const obj = {};
     for (const key of params.keys()) {
@@ -28,71 +28,73 @@ export default function PostsPage(props) {
   useEffect(() => {
     let params = parseParams(window.location.search);
     setQueryParam(params.tag);
-  }, [])
-
+  }, []);
 
   const loadMorePosts = () => {
-    Inertia.get(links.next, {}, {
-      preserveState: true,
-      preserveScroll: true,
-      only: ['posts'],
-      onSuccess: ({ props }) => {
-        setAllPosts([...allPosts, ...props.posts.data]);
-        window.history.replaceState({}, document.title, meta.path)
+    Inertia.get(
+      links.next,
+      {},
+      {
+        preserveState: true,
+        preserveScroll: true,
+        only: ["posts"],
+        onSuccess: ({props}) => {
+          setAllPosts([...allPosts, ...props.posts.data]);
+          window.history.replaceState({}, document.title, meta.path);
+        },
       }
-    });
-  }
+    );
+  };
 
   return (
     <Guest auth={auth.user}>
       <Head title={title} />
       <div className="min-h-screen">
-        {tags && tags.length > 0 &&
-          <div className="py-2 px-2 w-full lg:w-2/3 xl:w-5/6 lg:p-0 lg:py-2 lg:mx-auto">
-            <div className="flex flex-row flex-wrap bg-primary text-primary-content rounded-sm overflow-hidden">
-              <Link href={route('outer.main')} as="button" className="p-1"><FaArrowLeft /></Link>
+        {tags && tags.length > 0 && (
+          <div className="w-full py-2 px-2 lg:mx-auto lg:w-2/3 lg:p-0 lg:py-2 xl:w-5/6">
+            <div className="flex flex-row flex-wrap overflow-hidden rounded-sm bg-primary text-primary-content">
+              <Link href={route("outer.main")} as="button" className="p-1">
+                <FaArrowLeft />
+              </Link>
               <h5 className="p-1 italic">in trending</h5>
               {tags.map((trend, i) => {
                 return (
                   <span className="py-1" key={i}>
                     <a
                       name="tag"
-                      className={`${trend.hashtag == tagParam && 'bg-white text-black'} font-bold link px-2 hover:cursor-pointer hover:bg-white hover:text-black`}
+                      className={`${
+                        trend.hashtag == tagParam && "bg-white text-black"
+                      } link px-2 font-bold hover:cursor-pointer hover:bg-white hover:text-black`}
                       href={`/?tag=${trend.hashtag}`}>
                       #{trend.hashtag}
                     </a>
                   </span>
-                )
+                );
               })}
             </div>
           </div>
-        }
+        )}
         {posts.length > 0 ? (
-          <div className="py-2 px-2 w-full lg:w-2/3 xl:w-5/6 lg:p-0 lg:py-2 mx-auto">
-            <h1 className="font-sans font-bold text-2xl px-2 border-b-2 mb-4">Coders History</h1>
-            <div className="flex flex-col gap-2 md:gap-1 mb-16 lg:mb-10 md:mb-12">
-              <InfiniteScroll
-                dataLength={allPosts.length}
-                next={loadMorePosts}
-                hasMore={links.next !== null}
-                loader={
-                  <h4 style={{ textAlign: 'center' }}>
-                    Loading...
-                  </h4>
-                }
-                endMessage={
-                  <p style={{ textAlign: 'center' }}>
-                    <b>🌹...🌹</b>
-                  </p>
-                }
-              >
+          <div className="mx-auto w-full py-2 px-2 lg:w-2/3 lg:p-0 lg:py-2 xl:w-5/6">
+            <h1 className="mb-4 border-b-2 px-2 font-sans text-2xl font-bold">Coders History</h1>
+            <InfiniteScroll
+              dataLength={allPosts.length}
+              next={loadMorePosts}
+              hasMore={links.next !== null}
+              loader={<h4 style={{textAlign: "center"}}>Loading...</h4>}
+              endMessage={
+                <p style={{textAlign: "center"}}>
+                  <b>🌹...🌹</b>
+                </p>
+              }>
+              <div className="grid grid-cols-3">
                 <PostsList posts={allPosts} />
-              </InfiniteScroll>
-            </div>
+              </div>
+            </InfiniteScroll>
           </div>
         ) : (
           <div className="flex justify-center pt-5">
-            <div className="alert alert-warning w-11/12 md:w-1/3 rounded-sm text-slate-900 shadow-lg">
+            <div className="alert alert-warning w-11/12 rounded-sm text-slate-900 shadow-lg md:w-1/3">
               <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
